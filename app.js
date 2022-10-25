13b6b0db61ca7f12894bb49b4b2cf6879856b61d
sainath11-reddy <sainath11reddy@gmail.com>
2022-10-25 15:56:41 +0530

    first commit

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 // const db =require("./util/database");
 const sequelize =require("./util/database");
+const Product = require('./models/productSequelize');
+const User = require('./models/user');
+const Cart = require('./models/cartSequelize');
+const CartItem = require('./models/cart-item');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,12 +23,46 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use((req,res,next)=>{
+    User.findByPk(1).then(user =>{
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then((result)=>{
+Product.belongsTo(User, {constraints:true, onDelete:'CASCADE'});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through:CartItem});
+
+
+//Creating a Dummy User
+sequelize
+//  .sync({force:true})
+ .sync()
+ .then((result)=>{
     // console.log(result);
+    return User.findByPk(1);
+    
+}).then(user =>{
+    if(!user){
+        return User.create({name:"Sainath", email:"sainath11reddy@gmail.com"});
+    }
+    return user
+}).then(user =>{
+    // console.log(user);
+    return user.createCart()
+    
+}).then(cart =>{
     app.listen(3000);
-}).catch(err=>{console.log(err)});
+})
+.catch(err=>{
+    console.log(err)
+});
